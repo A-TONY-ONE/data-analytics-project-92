26b0d7aa4eaecc557da9e9fabdc9549b5d997911
A-TONY-ONE <101509861+A-TONY-ONE@users.noreply.github.com>
2023-07-01 16:14:34 +0500

    queries.sql

--- queries.sql
+++ queries.sql
@@ -59,3 +59,51 @@ LEFT JOIN products p ON
 GROUP BY
      s.sale_date,2, 1
 order by EXTRACT(ISODOW FROM s.sale_date);
+--6.1 Количество покупателей в разных возрастных группах: 16-25, 26-40 и 40+
+SELECT '16-25' as age_category, COUNT(age) as count 
+FROM customers 
+WHERE age BETWEEN 16 AND 25 
+UNION ALL 
+SELECT '26-40' as age_category, COUNT(age) as count 
+FROM customers 
+WHERE age BETWEEN 26 AND 40 
+UNION ALL 
+SELECT '40+' as age_category, COUNT(age) as count 
+FROM customers 
+WHERE age > 40;
+
+/*6.2 Во втором отчете предоставьте данные по количеству уникальных покупателей и выручке, которую они принесли. Сгруппируйте данные по дате, которая представлена в числовом виде ГОД-МЕСЯЦ. Итоговая таблица должна быть отсортирована по дате по возрастанию и содержать следующие поля:
+date - дата в указанном формате
+total_customers - количество покупателей
+income - принесенная выручка*/
+SELECT
+    to_char(s.sale_date, 'YYYY-MM') AS date,
+    count(DISTINCT (s.customer_id)) AS total_customers,
+    ROUND(sum(s.quantity * p.price), 3) AS income
+FROM
+    sales s
+LEFT JOIN products p ON
+    p.product_id = s.product_id
+GROUP BY
+   1
+ORDER BY
+    1 ASC ;
+
+/*6.3 Третий отчет следует составить о покупателях, первая покупка которых была в ходе проведения акций (акционные товары отпускали со стоимостью равной 0). Итоговая таблица должна быть отсортирована по id покупателя. Таблица состоит из следующих полей:
+customer - имя и фамилия покупателя
+sale_date - дата покупки
+seller - имя и фамилия продавца*/
+select DISTINCT on (c.customer_id)
+    CONCAT(c.first_name, ' ', c.last_name) AS customer,
+    to_char(s.sale_date, 'YYYY-MM-DD') AS sale_date,
+    CONCAT(e.first_name, ' ', e.last_name) AS seller
+FROM
+    sales s
+LEFT JOIN products p ON
+    p.product_id = s.product_id
+LEFT JOIN employees e ON
+    s.sales_person_id = e.employee_id 
+LEFT JOIN customers c on
+    s.customer_id = c.customer_id 
+WHERE p.price = 0
+ORDER by c.customer_id ASC;
